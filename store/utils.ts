97983a1affe0c3af21137bfd86b8a6f43b73cb38
ja153903 <jaimeabbariao@gmail.com>
2022-07-24 13:23:51 -0400

    fix: removing comment

--- store/utils.ts
+++ store/utils.ts
@@ -1,6 +1,5 @@
 import produce from 'immer';
 
-// given set => we want to decorate this with produce
 export function withImmer(zustandSetOrGetCallback: Function) {
   return function (fn: any) {
     return zustandSetOrGetCallback(produce(fn));
